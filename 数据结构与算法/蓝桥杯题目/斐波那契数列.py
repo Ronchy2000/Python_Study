8b1680a8fdad27fa5f8b9817da3d139ff6d501bc
Ronchy <rongqi1949@gmail.com>
2022-02-21 19:48:30 +0800

    2.21练习

---
+++ 数据结构与算法/蓝桥杯题目/斐波那契数列.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2022/2/21 19:41
+# @Author  : Ronchy_LU
+# @Email   : rongqi1949@gmail.com
+# @File    : 斐波那契数列.py
+# @Software: PyCharm
+'''
+1 1 2 3 5 8 13 21 34 .....
+求100以内的斐波那契数列
+'''
+a ,b = 1,1
+print(a,end='\n')
+print(b,end='\n')
+while(1):
+    c = a+b
+    a = b
+    b = c
+    if c >=100 :
+        break
+    print(c,end='\n')
+
