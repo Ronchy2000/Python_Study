91c6cb0a2d61d0d207f47d8b06731198e316c931
Ronchy <rongqi1949@gmail.com>
2022-07-14 15:44:15 +0800

    随机森林

---
+++ 机器学习/Sklearn_WorkSpace/随机森林/随机森林.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2022/7/13 17:10
+# @Author  : Ronchy_LU
+# @Email   : rongqi1949@gmail.com
+# @File    : 随机森林.py
+# @Software: PyCharm
+'''
+以决策树为基学习器构建Bagging集成的基础上，进一步在决策树训练的过程中引入了随机属性选择
+
+集成学习: ensemble learning  集成算法
+'''
+
+ 
+
+
