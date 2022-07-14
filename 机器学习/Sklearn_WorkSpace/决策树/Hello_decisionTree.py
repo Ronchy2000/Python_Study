9385ad79d177ff068523e0f50897e46efaa2a1af
Ronchy <rongqi1949@gmail.com>
2022-07-14 15:43:43 +0800

    决策树

--- 机器学习/Sklearn_WorkSpace/决策树/Hello_decisionTree.py
+++ 机器学习/Sklearn_WorkSpace/决策树/Hello_decisionTree.py
@@ -4,7 +4,12 @@
 # @Email   : rongqi1949@gmail.com
 # @File    : Hello_decisionTree.py
 # @Software: PyCharm
+'''
+决策树具有随机性
+random_state  设置分枝中的随机模式的参数
+,splitter,
 
+'''
 import pandas as pd
 import graphviz
 from sklearn import tree
@@ -34,7 +39,8 @@ Xtrain , Xtest , Ytrain , Ytest = train_test_split(wine.data,wine.target,test_si
 # print("Xtrain.shape:",Xtrain.shape)  #124行
 # print("wine.data.shape:",wine.data.shape)  #178行
 
-clf = tree.DecisionTreeClassifier(criterion="entropy")
+clf = tree.DecisionTreeClassifier(criterion="entropy",random_state=30 , splitter="random") #random_state， 随机选取特征
+# clf = tree.DecisionTreeClassifier(criterion="entropy")
 clf = clf.fit(Xtrain,Ytrain)
 score = clf.score(Xtest,Ytest)
 print("score:",score)
@@ -44,8 +50,18 @@ feature_name = ['alcohol', 'malic_acid', 'ash', 'alcalinity_of_ash', 'magnesium'
                ,'flavanoids', 'nonflavanoid_phenols', 'proanthocyanins', 'color_intensity', 'hue', 'od280/od315_of_diluted_wines', 'proline']
 dot_data = tree.export_graphviz(clf
                                 ,feature_names= feature_name
-                                ,class_names=["琴酒","雪莉","贝尔摩德"]
-                                ,filled=True
-                                ,rounded=True)
+                                ,class_names=["Gin","Sherry","Vermouth"]
+                                ,filled=True  #颜色
+                                ,rounded=True
+                                )
 graph = graphviz.Source(dot_data)
-print(graph)
+graph.view()   #查看流程图
+
+# print(clf.feature_importances_)
+print( [*zip(feature_name,clf.feature_importances_)]  )
+
+
+
+
+
+
