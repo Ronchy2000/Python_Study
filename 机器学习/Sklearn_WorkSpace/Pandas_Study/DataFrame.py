5761ea476d56e43652305dc9ec335618ae3d5019
Ronchy <rongqi1949@gmail.com>
2022-07-18 18:12:54 +0800

    2022.7.18

---
+++ 机器学习/Sklearn_WorkSpace/Pandas_Study/DataFrame.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2022/7/18 18:12
+# @Author  : Ronchy_LU
+# @Email   : rongqi1949@gmail.com
+# @File    : DataFrame.py
+# @Software: PyCharm
+
--- 机器学习/Sklearn_WorkSpace/Pandas_Study/Hello_Pandas.py
+++ 机器学习/Sklearn_WorkSpace/Pandas_Study/Hello_Pandas.py
@@ -4,3 +4,23 @@
 # @Email   : rongqi1949@gmail.com
 # @File    : Hello_Pandas.py
 # @Software: PyCharm
+
+import pandas as pd
+
+pd.options.display.max_rows = 9999 #大于9999 ，就缩略显示 ，否则就全部显示
+
+df = pd.read_csv('data.csv')
+# print(df.to_string())  #Tip: use to_string() to print the entire DataFrame.
+# print(df)
+
+print(df.head(10))  #打印前  10  行
+print("---------------------------------------")
+print(df.tail(10))  #打印后  10  行
+
+
+
+
+
+
+
+
