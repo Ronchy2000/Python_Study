daac021731407221517444c46c67fb5ad59c1a4d
Ronchy <rongqi1949@gmail.com>
2022-08-16 10:23:42 +0800

    2022.8.16

---
+++ 机器学习/Matrix Factorization/PMF_Rone/PMF_Rone.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2022/8/16 9:32
+# @Author  : Ronchy_LU
+# @Email   : rongqi1949@gmail.com
+# @File    : PMF_Rone.py
+# @Software: PyCharm
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+
+from sklearn.metrics import mean_squared_error
+
