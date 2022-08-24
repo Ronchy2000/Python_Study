5b6fc99018265960c8d14431536a74000e7b517b
Ronchy <rongqi1949@gmail.com>
2022-08-24 15:54:40 +0800

    2022.8.24

---
+++ 机器学习/Guassian Process Regression/2Dimen_GP.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2022/8/24 15:53
+# @Author  : Ronchy_LU
+# @Email   : rongqi1949@gmail.com
+# @File    : 2Dimen_GP.py
+# @Software: PyCharm
+
+#from: https://borgwang.github.io/ml/2019/07/28/gaussian-processes.html
