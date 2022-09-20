c90de3cf9db9ae239f2510cf397863843a08c8f3
ronchy <rongqi1949@gmail.com>
2022-09-20 21:11:40 +0800

    计算概率估计

--- 机器学习/BETA_Baysian Enhanced_STA/Baseline/BETA/分布概率.py
+++ 机器学习/BETA_Baysian Enhanced_STA/Baseline/BETA/分布概率.py
@@ -5,14 +5,17 @@
 # @File    : 分布概率.py
 # @Software: PyCharm
 
+import math
 import scipy.stats
 import numpy as np
+#使用-累积分布函数
 
-#均值
-mean = 23
-#标准差
-std = 8
+def prob_not_violation(X, mean, covariance):
+    # X = 0 # x<X  的概率
+    # mean = np.array([0,1,2,3,4,5])
+    # covariance = np.array([0.5, 1, 1.5, 2, 1.5, 2])
 
-#计算概率
-prob = scipy.stats.norm(mean, std).pdf(0)
-print("prob.", prob)
+    # prob of  x > X
+    prob = 1 - scipy.stats.norm(mean, covariance).cdf(X)
+    # print(prob)
+    return prob
---
+++ 机器学习/BETA_Baysian Enhanced_STA/Baseline/BETA/计算概率估计.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2022/9/20 21:10
+# @Author  : Ronchy_LU
+# @Email   : rongqi1949@gmail.com
+# @File    : 计算概率估计.py
+# @Software: PyCharm
+
+import pandas as pd
+import numpy as np
+
+
+
+
+
+
