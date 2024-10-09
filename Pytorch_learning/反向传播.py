2d9548ed79a68875652dc907fc1fdbb0d905141a
ronchy <rongqi1949@gmail.com>
2024-10-09 15:43:48 +0800

    modify all files

---
+++ Pytorch_learning/反向传播.py
@@ -0,0 +1,38 @@
+import torch
+import torchvision.datasets
+from torch import nn
+from torch.nn import Conv2d, Flatten, MaxPool2d
+from torch.utils.tensorboard import SummaryWriter
+
+dataset =torchvision.datasets.CIFAR10("./data", train=False, transform=torchvision.transforms.ToTensor(), download=True)
+class Seq(nn.Module):
+    def __init__(self):
+        super(Seq, self).__init__()
+        self.model1 = nn.Sequential(  #  上面的顺序模型，写入Sequential中，减少代码量。 在forward函数中直接调用model1即可
+            Conv2d(3, 32, 5, padding = 2),
+            MaxPool2d(kernel_size = 2, stride=2),
+            Conv2d(32, 32, 5, padding = 2),
+            MaxPool2d(kernel_size = 2, stride=2),
+            Conv2d(32, 64, 5, padding = 2),
+            MaxPool2d(kernel_size = 2, stride=2),
+            Flatten(start_dim=1,end_dim=-1),
+            nn.Linear(1024, 64),
+            nn.Linear(64, 10)
+        )
+
+    def forward(self, x):
+        x = self.model1(x)
+        return x
+
+seq = Seq()
+
+if __name__ == '__main__':
+    # print("seq_module:", seq)
+    input = torch.ones((64,3,32,32))
+    # print("input:", input)
+    output = seq(input)
+    print("output:", output.shape)
+
+    writer = SummaryWriter("./logs/logs_seq")
+    writer.add_graph(seq, input)
+    writer.close()
